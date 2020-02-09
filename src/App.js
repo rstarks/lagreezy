f45795e11bdaf293207c707bcc48117d80a815fe
Rory <rory.starks@gmail.com>
2020-02-09 14:57:33 -0800

    Added moves and rendering full move cards
    Basic styling added, full moveset rendered with all data

--- src/App.js
+++ src/App.js
@@ -1,16 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import moveData from "./data/moves.json";
+
+class App extends Component {
+  constructor() {
+    super();
+    // Update the state with jsonData
+    this.state = {
+      moveData
+    };
+    this.listItems = this.listItems.bind(this);
+  }
+
+  // Refactor later into something more interesting
+  listItems(list) {
+
+    return (list.length > 0) ? list.join(', ') : '';
+  }
+
+  checkBoolean(value) {
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.moveData.moves.map(move => (
+          /* Move Card */
+          <div className='move-card' key={move.id}>
+            {/* Name */}
+            <div style={{fontWeight:'bold'}}>{move.name}</div>
+
+            <div>Block: {move.block}</div>
+
+            <div>Yellow Springs: {move.yellow_springs}</div>
+            <div>Red Springs: {move.red_springs}</div>
+
+            <div>{move.black_cables ? 'Black Cables' : '' }</div>
+            <div>{move.red_cables ? 'Red Cables' : '' }</div>
+
+            <div className='line-break'>Location: {this.listItems(move.location)}</div>
+
+            <div>Direction Facing:{move.direction}</div>
+
+            <div>Duration: {move.duration}:00</div>
+
+            <div className='line-break'>Primary Muscles: {this.listItems(move.primary_muscles)}</div>
+
+            <div className='line-break'>Secondary Muscles: {this.listItems(move.secondary_muscles)}</div>
+
+            <div>{move.level}</div>
+
+            <div className='line-break'>Action: {this.listItems(move.action)}</div>
+            <div className='line-break'>Related Moves: {this.listItems(move.related)}</div>
 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        fdsafsd
-      </header>
-    </div>
-  );
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
